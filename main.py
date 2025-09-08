b1aa1dddd30d8a0d2c7bf1de2d9623ddb5b6e158
NicolasTibacan <jntibacans@academia.usbbog.edu.co>
2025-09-08 09:17:26 -0500

    cambios en el main

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+from controller.controller import app
+from Config.databse import init_db
+
+if __name__ == "__main__":
+    # Inicializar la base de datos (crea tablas si no existen)
+    init_db()
+
+    # Ejecutar la aplicación Flask
+    app.run(host="0.0.0.0", port=5000, debug=True)
